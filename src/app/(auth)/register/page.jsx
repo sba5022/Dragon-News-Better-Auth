7545e7e424b12171cab5cc5dde8da2acb121ee4b
sba5022 <sumaiaofficial02@gmail.com>
2026-05-24 19:59:42 +0600

    register design

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,9 +1,51 @@
+'use client'
+import Link from 'next/link';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const RegisterPage = () => {
+    const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+    const handleRegisterFunc = (data)=> {
+console.log(data,"data");
+const {name,photoUrl,email,password} = data;
+console.log(name,photoUrl,email,password,"register data");
+    }
+   
     return (
-        <div>
-            Register
+        <div className="Container mx-auto min-h-[80vh] flex items-center justify-center bg-slate-100" >
+            <div className="p-4 rounded-xl bg-white">
+<h2 className="text-3xl font-bold text-center mb-4">Register your account</h2>
+<form className="space-y-4" onSubmit={  handleSubmit(handleRegisterFunc)}>
+     <fieldset class="$$fieldset">
+  <legend class="$$fieldset-legend font-semibold ">Your Name</legend>
+  <input {...register("name",{ required: "Name is required" })}  type="text" class="$$input w-full" placeholder="Type here your name" />
+  {errors.name && <p className="text-red-500">Name is required</p>}
+</fieldset>
+  <fieldset class="$$fieldset">
+  <legend class="$$fieldset-legend font-semibold ">Photo URL</legend>
+  <input {...register("photoUrl",{ required: "Photo URL is required" })}  type="text" class="$$input w-full" placeholder="Type here your photo URL" />
+  {errors.photoUrl && <p className="text-red-500">Photo URL is required</p>}
+</fieldset>
+    <fieldset class="$$fieldset">
+  <legend class="$$fieldset-legend font-semibold ">Email address</legend>
+  <input {...register("email",{ required: "Email is required" })}  type="email" class="$$input w-full" placeholder="Type here your email" />
+  {errors.email && <p className="text-red-500">Email is required</p>}
+</fieldset>
+  <fieldset class="$$fieldset">
+  <legend class="$$fieldset-legend font-semibold ">Password</legend>
+  <input {...register("password",{ required: "Password is required" })}  type="password" class="$$input w-full" placeholder="Type here your password" />
+{errors.password && <p className="text-red-500">Password is required</p>}
+
+</fieldset>
+<button className="btn btn-neutral w-full">Register</button>
+</form>
+<p>Already Have An Account ? <Link href="/login" className='text-red-600'>Login</Link></p>
+            </div>
+            
         </div>
     );
 };
